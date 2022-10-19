aea59bf972cac361c23e9b89d4314106947a36fb
danylokl <user>
2022-10-20 00:14:22 +0300

    Add project files.

---
+++ Books_Blazor/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using Books_Blazor.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Books_Blazor.Models
+{
+    public class Book
+    {
+        [Required]
+        public string Title { get; set; }
+        public string Cover { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public List<Review> Reviews { get; set; }
+    }
+}
